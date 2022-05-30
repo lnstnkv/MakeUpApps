c49715a8bb6a20b19547c91e4d4a81ef79744d5c
Elena Sitnikova <lnstnkv2002@gmail.com>
2022-05-30 15:29:03 +0700

    initial commit

---
+++ app/src/main/java/com/tsu/makeupapps/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.tsu.makeupapps
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
