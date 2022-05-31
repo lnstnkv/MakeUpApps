fbce43696801acf429cd1ef2a48c358ce6cc1897
Elena Sitnikova <lnstnkv2002@gmail.com>
2022-05-31 13:23:28 +0700

    Сверстать экран регистрации
    Создан экран регистрации,добавлены новые стили для EditText, не хватает селектора для кнопко

---
+++ app/src/main/java/com/tsu/makeupapps/RegisterActivity.kt
@@ -0,0 +1,11 @@
+package com.tsu.makeupapps
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class RegisterActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_register)
+    }
+}
